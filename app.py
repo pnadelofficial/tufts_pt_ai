0af8124f05d9e1af0c1cd2abb65b00913012dba3
peter nadel <peter.m.nadel@gmail.com>
2024-05-01 13:05:04 -0400

    parsing

--- app.py
+++ app.py
@@ -28,11 +28,12 @@ if st.button('Start Chat', on_click=clear_state):
     with st.spinner('Generating questions...'):
         chat()
 
+    counter = 0
     for q in st.session_state['questions']:
-        st.header(f'Question {st.session_state["questions"].index(q)+1}')
         _split = [s for s in re.split(r'([A-Z]+:)|([A-Z]+\.)', q)[1:] if s is not None]
         to_display = '\n'.join(['* '+''.join([s, _split[i+1]]) for i, s in enumerate(_split[:-1]) if i % 2 == 0])
-        print(to_display)
         if to_display:
+            st.header(f'Question {counter+1}')
             st.markdown(to_display)
+            counter += 1
     st.download_button('Download questions as a CSV', pd.DataFrame(st.session_state['questions']).to_csv(), f'questions.csv', 'text/csv')
